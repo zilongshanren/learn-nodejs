ecf67684429061f51dbf215cb61e252b11672d3a
zilongshanren <guanghui8827@126.com>
2015-09-21 11:32:35 +0800

    add help links

--- common-js-test/program.js
+++ common-js-test/program.js
@@ -1,5 +1,7 @@
 /*global exports, require */
 // include module must be a . or .. / [module name]
+//http://0fps.net/2013/01/22/commonjs-why-and-how/
+//http://www.commonjs.org/specs/modules/1.0/
 var inc = require('./increment').increment;
 var a = 1;
 console.log(inc(a));
