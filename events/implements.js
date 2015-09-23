0d6efbb6874b87ef9ba50fdba1066caf6590cea2
zilongshanren <guanghui8827@126.com>
2015-09-23 13:35:18 +0800

    add node-workshop

--- events/implements.js
+++ events/implements.js
@@ -1,18 +1,17 @@
-var utils = require('util');
-
+var util = require('util');
 var EventEmitter = require('events').EventEmitter;
 
-function HeartBeat(interval) {
+function Heartbeat(interval){
     EventEmitter.call(this);
 
     var emitter = this;
     var beats = 0;
 
-    setInterval(function() {
-        emitter.emit('beat' + beats);
+    setInterval(function(){
+        emitter.emit('beat', ++beats);
     }, interval);
 }
 
-utils.inherits(HeartBeat, EventEmitter);
+util.inherits(Heartbeat, EventEmitter);
 
-module.exports = HeartBeat;
+module.exports = Heartbeat;
