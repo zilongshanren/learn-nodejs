2651a5d9c389d99e783e8a45ac70375532e82302
zilongshanren <guanghui8827@126.com>
2015-10-13 18:15:22 +0800

    finish the test

--- mocha/lib/tags.js
+++ mocha/lib/tags.js
@@ -1,62 +1,62 @@
 exports = module.exports = {};
 exports.parse = function(args, defaults, replacements) {
-        var options = {};
-        if (typeof defaults === 'object' && !(defaults instanceof Array)) {
-            options = defaults;
-        }
+    var options = {};
+    if (typeof defaults === 'object' && !(defaults instanceof Array)) {
+        options = defaults;
+    }
 
-        if (typeof replacements === 'object' && !(replacements instanceof Array)) {
-            for (var i in args) {
-                var arg = args[i];
-                if (arg.charAt(0) === '-' && arg.charAt(1) !== '-') {
-                    arg = arg.substr(1);
-                    if (arg.indexOf('=') !== -1) {
-                        arg = arg.split('=');
-                        var keys = arg.shift();
-                        var value = arg.join('=');
+    if (typeof replacements === 'object' && !(replacements instanceof Array)) {
+        for (var i in args) {
+            var arg = args[i];
+            if (arg.charAt(0) === '-' && arg.charAt(1) !== '-') {
+                arg = arg.substr(1);
+                if (arg.indexOf('=') !== -1) {
+                    arg = arg.split('=');
+                    var keys = arg.shift();
+                    var value = arg.join('=');
 
-                        arg = keys.split('');
-                        var key = arg.pop();
-                        if (replacements.hasOwnProperty(key)) {
-                            key = replacements[key];
-                        }
-
-                        args.push('--' + key + '=' + value);
-                    } else {
-                        arg = arg.split('');
+                    arg = keys.split('');
+                    var key = arg.pop();
+                    if (replacements.hasOwnProperty(key)) {
+                        key = replacements[key];
                     }
 
-                    arg.forEach(function(key) {
-                        if (replacements.hasOwnProperty(key)) {
-                            key = replacements[key];
-                        }
-                        args.push('--' + key);
-                    });
+                    args.push('--' + key + '=' + value);
+                } else {
+                    arg = arg.split('');
                 }
+
+                arg.forEach(function(key) {
+                    if (replacements.hasOwnProperty(key)) {
+                        key = replacements[key];
+                    }
+                    args.push('--' + key);
+                });
             }
-        };
+        }
+    }
 
 
     for (var i in args) { //Cycle through args
         var arg = args[i];
         //Check if Long formed tag
-        if (arg.substr(0, 2) === "--") {
+        if (arg.substr(0, 2) === '--') {
             arg = arg.substr(2);
             //Check for equals sign
-            if (arg.indexOf("=") !== -1) {
-                arg = arg.split("=");
+            if (arg.indexOf('=') !== -1) {
+                arg = arg.split('=');
                 var key = arg.shift();
 
-                var value = arg.join("=");
-                
+                var value = arg.join('=');
+
                 if (/^[0-9]+$/.test(value)) {
                     value = parseInt(value, 10);
                 }
                 options[key] = value;
-            }else{
+            } else {
                 options[arg] = true;
             }
         }
     }
-    return options; 
-}
+    return options;
+};
